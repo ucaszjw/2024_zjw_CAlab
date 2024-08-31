cfe9b5ada033d4b3a5e9c40573ac5683ae2a22e4
ucaszjw <zhangjiawei2004@126.com>
2024-08-31 19:36:22 +0800

    finish prj1 report

--- prj1/code/mycpu_top.v
+++ prj1/code/mycpu_top.v
@@ -70,7 +70,8 @@ wire [ 1:0] op_21_20;
 wire [ 4:0] op_19_15;
 wire [ 4:0] rd;
 wire [ 4:0] rj;
-wire [ 4:0] rk,i5;
+wire [ 4:0] rk;
+wire [ 4:0] i5;
 wire [11:0] i12;
 wire [19:0] i20;
 wire [15:0] i16;
@@ -229,7 +230,7 @@ assign src2_is_imm   = inst_slli_w |
 
 assign res_from_mem  = inst_ld_w;
 assign dst_is_r1     = inst_bl;
-assign gr_we         = (~inst_st_w & ~inst_beq & ~inst_bne & ~inst_b & ~inst_bl) | inst_bl;
+assign gr_we         = ~inst_st_w & ~inst_beq & ~inst_bne & ~inst_b;
 assign mem_we        = inst_st_w;
 assign dest          = dst_is_r1 ? 5'd1 : rd;
 
